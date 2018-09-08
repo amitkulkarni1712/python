1ebc8a40598efeb04e99521d759442fec3f45803
amitkulkarni1712 <amitkulkarni@djalexnder.in>
2018-09-08 12:33:29 +0530

    Sat 8th Sept list Functions

---
+++ 10listfunt.py
@@ -0,0 +1,47 @@
+list1=['10','12','35','14','53','10']
+list2=["x","y","z"]
+list3=[1,2,3,4,5.4,6]
+
+#list1.append (list2)
+
+#list3=list1.copy()
+
+#print(list3)
+
+#list3.extend(list1)
+#print(list3)
+
+#list3.clear()
+#print(list3)
+
+#list3.pop()
+#print(list3)
+
+#list3.extend(range(2,10))
+
+#print(list3)
+
+#list1.insert(-1,"11")
+
+#list1.remove("10")
+
+
+#print(list1)
+
+#c=list1.count("10")
+
+
+#print(c)
+
+
+#while c>0:
+#    list1.remove("10")
+#    c=c-1
+#print(list1)
+
+s=sum(list3)
+print(s)
+
+s1=round(list3[4])
+
+print(s1)
