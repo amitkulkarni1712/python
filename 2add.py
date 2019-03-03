8de6f02699583adfba1211394e8e83889d36c2ac
amitkulkarni1712 <a.kulkarni888@gmail.com>
2019-03-03 23:44:08 +0530

    String function

--- 2add.py
+++ 2add.py
@@ -1,4 +1,12 @@
+## Working with numbers
 x=10
 y=20
-
 print(x+y)
+
+print(10 % 3)
+
+# print numner as string
+
+my_num = 5
+print(str(my_num) + " Is my fav number")
+################################################
