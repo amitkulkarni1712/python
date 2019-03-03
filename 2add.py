76fce5f934c082fc6df06e0b2bf072b41d3e93fc
amitkulkarni1712 <a.kulkarni888@gmail.com>
2019-03-04 00:01:10 +0530

    new files added

--- 2add.py
+++ 2add.py
@@ -2,11 +2,14 @@
 x=10
 y=20
 print(x+y)
-
 print(10 % 3)
-
 # print numner as string
-
 my_num = 5
 print(str(my_num) + " Is my fav number")
 ################################################
+# Power function
+print(pow(4, 19))
+## bigger number
+print(max(10, 12))
+## Smaller number
+print(min(18, 5))
---
+++ math.py
@@ -0,0 +1,6 @@
+from math import *
+
+my_name = 5
+print(ceil(3.7))
+## square root
+print(sqrt(81))
