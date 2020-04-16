4e10b4f3ad70ee8c091b883f026ee2e21cb253d8
amitkulkarni1712 <amitkulkarni@djalexander.in>
2020-04-16 21:17:35 +0530

    Operators 16th April 2020

---
+++ python-class/Operators/Arithemetic-operators.py
@@ -0,0 +1,23 @@
+# Addition
+a = 13 + 5
+print(a)
+# Sub
+b = 13 - 5
+print(b)
+# Mul
+c = 13 * 5
+print(c)
+# Div
+d = 13/5
+print(d)
+
+# Modulus
+e = 13%5
+print(e)
+
+# exponent
+f = 13**5
+print(f)
+# Integer division
+g = 13//5
+print(g)
---
+++ python-class/Operators/Assignment-operators.py
@@ -0,0 +1,35 @@
+x = 20
+y = 10
+z = 5
+
+# Addition Assignment
+z+=x
+print(z)
+
+# sub Assignment
+z-=x
+print(z)
+
+# Mul Assignment
+z*=x
+print(z)
+
+# Division Assignment
+z/=x
+print(z)
+
+# Modulus Assignment
+z%=x
+print(z)
+
+# Exponential Assignment
+z**=y
+print(z)
+
+# Integer  Assignment
+z//=x
+print(z)
+
+
+n = 50
+print(-n)
