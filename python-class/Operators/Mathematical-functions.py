ab8d22962e623bb0e08d26e7f8060c856d82e9ff
amitkulkarni1712 <amitkulkarni@djalexander.in>
2020-04-20 20:35:13 +0530

    Identity & Math Module

---
+++ python-class/Operators/Mathematical-functions.py
@@ -0,0 +1,12 @@
+import math
+
+x = math.sqrt(25)
+
+print(x)
+
+### Area of Circle
+# r=piR2
+r = 17.5
+area = math.pi * r ** 2
+print("Area of circle = ", area)
+
---
+++ python-class/Operators/identity.py
@@ -0,0 +1,8 @@
+a = 28
+b = 28
+
+c = id(a)
+print(c)
+
+d = id(b)
+print(d)
