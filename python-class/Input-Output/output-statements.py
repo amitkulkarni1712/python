24e6e2aa9fb29e05d731c53cb387d8dc13371c0d
amitkulkarni1712 <amitkulkarni@djalexander.in>
2020-04-21 20:45:07 +0530

    Input & output

---
+++ python-class/Input-Output/output-statements.py
@@ -0,0 +1,36 @@
+print("Hello")
+# For new Line
+print(" This s first line  \n This is the second line")
+
+# For tab
+print("Amit\tkulkarni")
+
+# Repetition operator
+print(3*'\ttejas')
+
+# To concatenation
+print("City name="+"Pune")
+
+# To print (variable list) statement
+
+a, b = 2, 4
+print(a)
+print(a, b)
+
+print(a, b, sep=",")
+print(a, b, sep=":")
+print(a, b, sep="--------")
+
+# Printing the Object statements
+lst = [10, 'A', 'Pankaj']
+print(lst)
+
+# Formatted string
+name = 'pankaj'
+
+print('Hello %20s' %name)
+
+n1, n2, n3 = 1, 2, 3
+print('number1={1}'.format(n1, n2, n3))
+
+#----------------------------------------------
