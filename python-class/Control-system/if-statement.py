636e37a11d456ada8be58af3e45ec9ef59a124fd
amitkulkarni1712 <amitkulkarni@djalexander.in>
2020-04-23 20:28:47 +0530

    Control system

---
+++ python-class/Control-system/if-statement.py
@@ -0,0 +1,11 @@
+## We are writing this programe for if statement
+
+#num = 1
+#if num==1:
+#    print("A")
+
+str = 'Yes'
+#num = int(input("Enter a number : "))
+#if num == 1:
+#    print(" Yes  \n We are learning if statement")
+
